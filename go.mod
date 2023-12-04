41f938c6ee3a55f024bf44b9d11d4b6d124878c5
Baiju Muthukadan <baiju.m.mail@gmail.com>
2023-12-04 17:20:30 +0530

    Add basic code to expose an API endpoint

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/operational-intelligence/logilica-integration
+
+go 1.21.4
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/urfave/negroni v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+func HandleWebhook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"test": "ok}`))
+}
+
+func route() (n *negroni.Negroni, rt *mux.Router) {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", HandleWebhook).Methods("GET")
+	n = negroni.New(negroni.NewRecovery(), negroni.NewLogger())
+	return n, router
+}
+func main() {
+
+	n, router := route()
+	n.UseHandler(router)
+	n.Run(":8080")
+
+}
